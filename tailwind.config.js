749a21b848c074a81552f23e0f3268b7ca9de69d
AmirSh <amirsh.webdev@gmail.com>
2022-10-20 21:37:12 +0330

    theme changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor: {
-        "primary": "rgba(255,0,0)"
+        "primary": "rgba(255,0,0)",
+        "secondary": "rgba(64,64,64)"
       },
       textColor: {
         "white": "#ececec",
         "primary": "rgb(239 68 68)",
-        "secondary": "rgba(64,64,64)"
       },
       backgroundImage: {
         "building": "linear-gradient(rgba(0,0,0,0.75), rgba(0,0,0,0.75)), url(../assets/building-bg.jpg)"
